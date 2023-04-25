0855bf92e9d6df6780e99e3a4809954b76d20841
isaaclyra <isaac.lyra.junior@gmail.com>
2023-04-25 10:39:29 -0300

    refactor: change movie name

--- db/db.go
+++ db/db.go
@@ -3,7 +3,7 @@ package db
 import "api-go/models"
 
 var Filmes = []models.Filme{
-	{ID: "1", Titulo: "Hotel Transilvânia 2", Direcao: "Genndy Tartakovsky, Jennifer Kluska, Derek Drymon", Producao: "Sony Pictures Animation", AnoLancamento: 2012},
+	{ID: "1", Titulo: "Hotel Transilvânia", Direcao: "Genndy Tartakovsky, Jennifer Kluska, Derek Drymon", Producao: "Sony Pictures Animation", AnoLancamento: 2012},
 	{ID: "2", Titulo: "Tá Dando Onda", Direcao: "Ash Brannon, Chris Buck", Producao: "Sony Pictures Animation", AnoLancamento: 2007},
 	{ID: "3", Titulo: "Interestelar", Direcao: "Christopher Nolan", Producao: "Legendary Pictures, Syncopy Films, Lynda Obst Productions", AnoLancamento: 2014},
 	{ID: "4", Titulo: "Vingadores: Ultimato", Direcao: "Anthony Russo, Joe Russo", Producao: "Marvel Studios", AnoLancamento: 2019},
