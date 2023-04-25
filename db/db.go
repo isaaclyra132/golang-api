6c88f09ca08904679545ecd3eb8bb7c64d81ae5f
isaaclyra <isaac.lyra.junior@gmail.com>
2023-04-25 11:16:48 -0300

    refactor: comment movies

--- db/db.go
+++ db/db.go
@@ -7,6 +7,6 @@ var Filmes = []models.Filme{
 	{ID: "2", Titulo: "Tá Dando Onda", Direcao: "Ash Brannon, Chris Buck", Producao: "Sony Pictures Animation", AnoLancamento: 2007},
 	{ID: "3", Titulo: "Interestelar", Direcao: "Christopher Nolan", Producao: "Legendary Pictures, Syncopy Films, Lynda Obst Productions", AnoLancamento: 2014},
 	{ID: "4", Titulo: "Vingadores: Ultimato", Direcao: "Anthony Russo, Joe Russo", Producao: "Marvel Studios", AnoLancamento: 2019},
-	{ID: "5", Titulo: "Coringa", Direcao: "Todd Phillips", Producao: "Village, Roadshow Pictures, DC Films, Sikelia Productions, Joint Effort Productions, Green Hat Films", AnoLancamento: 2019},
-	{ID: "6", Titulo: "Coringa 2", Direcao: "Todd Phillips", Producao: "Village, Roadshow Pictures, DC Films, Sikelia Productions, Joint Effort Productions, Green Hat Films", AnoLancamento: 2023},
+	// {ID: "5", Titulo: "Coringa", Direcao: "Todd Phillips", Producao: "Village, Roadshow Pictures, DC Films, Sikelia Productions, Joint Effort Productions, Green Hat Films", AnoLancamento: 2019},
+	// {ID: "6", Titulo: "Coringa 2", Direcao: "Todd Phillips", Producao: "Village, Roadshow Pictures, DC Films, Sikelia Productions, Joint Effort Productions, Green Hat Films", AnoLancamento: 2023},
 }
